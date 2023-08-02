0c865eeafcf56d95f07d262e3f8f31ba1ab0cd9e
Amandaa-S <amanda.salinas@sansano.usm.cl>
2023-08-02 09:44:11 -0700

    load model

--- hello_tf.c
+++ hello_tf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <tensorflow/c/c_api.h>
 
+#define INPUT_SIZE 50
+
+
 int main() {
   printf("Hello from TensorFlow C library version %s\n", TF_Version());
 
@@ -9,5 +12,20 @@ int main() {
   TF_Status* status = TF_NewStatus();
   TF_SessionOptions* session_options = TF_NewSessionOptions();
   TF_Buffer* run_options = NULL;
+  const char* tags = "serve";
+
+  TF_Session* session = TF_LoadSessionFromSavedModel(
+        session_options, run_options, "modelo", &tags, 1, graph, NULL, status
+  );
+
+  if (TF_GetCode(status) != TF_OK) {
+    fprintf(stderr, "Error al cargar el modelo: %s", TF_Message(status));
+    return 1;
+  }
+  //input
+  float input[INPUT_SIZE];
+  for (int i = 0; i < INPUT_SIZE; i++) {
+      input[i] = i + 1;
+  }
   return 0;
 }
