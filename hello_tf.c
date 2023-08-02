c641fbc0ba0c825bb6b3391b75b108e192fa694d
Amandaa-S <amanda.salinas@sansano.usm.cl>
2023-08-02 09:46:52 -0700

    update

--- hello_tf.c
+++ hello_tf.c
@@ -8,20 +8,20 @@ int main() {
   printf("Hello from TensorFlow C library version %s\n", TF_Version());
 
   // Cargar el modelo SavedModel
-  TF_Graph* graph = TF_NewGraph();
-  TF_Status* status = TF_NewStatus();
-  TF_SessionOptions* session_options = TF_NewSessionOptions();
-  TF_Buffer* run_options = NULL;
-  const char* tags = "serve";
+  // TF_Graph* graph = TF_NewGraph();
+  // TF_Status* status = TF_NewStatus();
+  // TF_SessionOptions* session_options = TF_NewSessionOptions();
+  // TF_Buffer* run_options = NULL;
+  // const char* tags = "serve";
 
-  TF_Session* session = TF_LoadSessionFromSavedModel(
-        session_options, run_options, "modelo", &tags, 1, graph, NULL, status
-  );
+  // TF_Session* session = TF_LoadSessionFromSavedModel(
+  //       session_options, run_options, "modelo", &tags, 1, graph, NULL, status
+  // );
 
-  if (TF_GetCode(status) != TF_OK) {
-    fprintf(stderr, "Error al cargar el modelo: %s", TF_Message(status));
-    return 1;
-  }
+  // if (TF_GetCode(status) != TF_OK) {
+  //   fprintf(stderr, "Error al cargar el modelo: %s", TF_Message(status));
+  //   return 1;
+  // }
   //input
   float input[INPUT_SIZE];
   for (int i = 0; i < INPUT_SIZE; i++) {
